10402be7fac4e70a3f077c8f15de4e1c1e182d0b
Marta Rey <martarey@MBP-de-Marta.home>
2020-07-20 18:01:33 +0200

    feat: new useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data);
+        setLoading(false);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    fetchData();
+  }, [url]);
+
+  return {data, loading};
+
+};
+
+export default useFetch;
