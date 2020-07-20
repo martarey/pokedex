7d1a89672874a71a5fc19f921a626f47a8b96e5e
Marta Rey <martarey@MBP-de-Marta.home>
2020-07-20 18:02:29 +0200

    feat: use hook useFetch in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { API_URL } from './conf/consts';
+import useFetch from './hooks/useFetch';
 import 'App.scss';
 
 function App() {
+  const { data, loading } = useFetch(API_URL);
+  if (loading) {
+    return (<p>Cargando Pokemons...</p>);
+  }
+  console.log(data);
   return (
     <h1>Hola, Mundo</h1>
   );
