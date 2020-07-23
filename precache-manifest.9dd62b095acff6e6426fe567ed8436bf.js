966dd5a14b3d9e27a7400ae0451cc57de865be4c
Marta Rey <martarey@MBP-de-Marta.home>
2020-07-23 11:24:09 +0200

    Updates

---
+++ precache-manifest.9dd62b095acff6e6426fe567ed8436bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df05b25b5dcac14cbcc526d4c887228e",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "36711ca8eaebf8d8afd7",
+    "url": "/pokedex/static/css/main.047d96c6.chunk.css"
+  },
+  {
+    "revision": "c4e5f7834878cca3d847",
+    "url": "/pokedex/static/js/2.07385e8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.07385e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36711ca8eaebf8d8afd7",
+    "url": "/pokedex/static/js/main.f5db897a.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
